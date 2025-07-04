aac0a410900daf0bcd5800c1ec36d4280940884a
Maxima <MaximaRasskazov@gmail.com>
2025-07-04 23:44:06 +0500

    Обновиил main после переноса функций

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"log"
-	"os"
-	"path/filepath" // Работа с путем к логам: Папка, Файлы
+	"os" // Работа с путем к логам: Папка, Файлы
 	"strings"
 )
 
 // сделать вывод в файл с указанным именем
-
-// игнор регистра ключевых слов error ERROR
 
 func main() {
 	dir, keywords := parseFlags()
@@ -19,6 +15,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("Ошибка: %v", err)
 	}
+}
+
+// Объявляем "Флаги" для CLI - командной строки,
+// Далее "Парсим" ввод пользователя
+func parseFlags() (string, []string) {
+	dir := flag.String("dir", "test-logs", "Директория с логами")
+	keywords := flag.String("keywords", "ERROR", "Ключевые слова для поиска")
+	flag.Parse()
+
+	return *dir, strings.Split(*keywords, ",")
 }
 
 // Основная логика программы
@@ -55,118 +61,4 @@ func run(dir string, keywords []string) error {
 	close(resultChan)
 
 	return nil
-}
-
-// Объявляем "Флаги" для CLI - командной строки,
-// Далее "Парсим" ввод пользователя
-func parseFlags() (string, []string) {
-	dir := flag.String("dir", "test-logs", "Директория с логами")
-	keywords := flag.String("keywords", "ERROR", "Ключевые слова для поиска")
-	flag.Parse()
-
-	return *dir, strings.Split(*keywords, ",")
-}
-
-// Сканирование директории,
-// которую укзывают в командой строке
-func scanDirectory(dir string) ([]os.DirEntry, error) {
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	return files, nil
-}
-
-// Обработка одного файла
-func processFile(dir string, file os.DirEntry, keywords []string) error {
-	if file.IsDir() {
-		return nil
-	}
-
-	fullPath := filepath.Join(dir, file.Name())
-	openedFile, err := os.Open(fullPath)
-	if err != nil {
-		return err
-	}
-	defer openedFile.Close()
-
-	readFileContent(openedFile, keywords, file.Name())
-
-	return nil
-}
-
-func readFileContent(file *os.File, keywords []string, filename string) {
-	scanner := bufio.NewScanner(file)
-	lineNumber := 0
-
-	for scanner.Scan() {
-		lineNumber++
-		line := scanner.Text()
-		analyzeLine(line, filename, keywords, lineNumber)
-	}
-
-	// if err := scanner.Err(); err != nil {
-	// 	fmt.Errorf("ошибка сканирования %s: %w", filename, err)
-	// }
-}
-
-func analyzeLine(line, fileName string, keywords []string, lineNumber int) {
-	for _, kw := range keywords {
-		keyword := strings.TrimSpace(kw)
-		if keyword == "" {
-			continue
-		}
-
-		keywordLower := strings.ToLower(keyword)
-		lineLower := strings.ToLower(line)
-		keywordLen := len(keywordLower)
-
-		if keywordLen == 0 {
-			continue
-		}
-
-		start := 0
-		for {
-			// Ищем вхождение в подстроке (начиная с позиции start)
-			idx := strings.Index(lineLower[start:], keywordLower)
-			if idx == -1 {
-				break
-			}
-
-			// Реальная позиция в оригинальной строке
-			realIdx := start + idx
-			foundWord := line[realIdx : realIdx+keywordLen]
-			keywordColor := colorForKeyword(keyword)
-
-			log.Printf(
-				"Найдено %s[%s]\033[0m в файле [%s]\033[0m на строке [%d]",
-				keywordColor,
-				foundWord,
-				fileName,
-				lineNumber,
-			)
-
-			// Перемещаем позицию поиска
-			start = realIdx + keywordLen
-
-			// Проверяем выход за границы строки
-			if start >= len(lineLower) {
-				break
-			}
-		}
-	}
-}
-
-func colorForKeyword(key string) string {
-	switch strings.ToUpper(key) {
-	case "ERROR":
-		return "\033[31m" // Красный
-	case "WARNING":
-		return "\033[33m" // Жёлтый
-	case "INFO":
-		return "\033[32m"
-	default:
-		return "\033[0m" // Сброс
-	}
 }
